67b8d61766540c2b2c99b741e015bb6ea03de61d
philliparthur98-ops <philliparthur98@icloud.com>
2026-09-11 14:43:32 -0500

    Add Unreal game target

---
+++ unreal/Source/BlacksiteExtraction.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class BlacksiteExtractionTarget : TargetRules
+{
+    public BlacksiteExtractionTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+        ExtraModuleNames.Add("BlacksiteExtraction");
+    }
+}
