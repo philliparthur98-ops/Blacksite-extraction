ca5dc60bd7f799f90154f8cda2edba2cfaf05a8f
philliparthur98-ops <philliparthur98@icloud.com>
2026-09-11 14:51:18 -0500

    Add archive objective actor

---
+++ unreal/Source/BlacksiteRuntime/BlacksiteObjective.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BlacksiteObjective.generated.h"
+
+class ABlacksitePlayerCharacter;
+class UPointLightComponent;
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class BLACKSITERUNTIME_API ABlacksiteObjective : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    ABlacksiteObjective();
+    void Interact(ABlacksitePlayerCharacter* Player);
+
+private:
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<USceneComponent> Root;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UStaticMeshComponent> DriveCase;
+
+    UPROPERTY(VisibleAnywhere)
+    TObjectPtr<UPointLightComponent> MarkerLight;
+
+    bool bSecured = false;
+};
